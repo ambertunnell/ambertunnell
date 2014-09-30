8e39d4af406b4d5347738b42ce08e6a177ee897c
Amber Tunnell <amber.tunnell@gmail.com>
2014-09-30 13:04:57 -0400

    trying on heroku again

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -15,11 +15,11 @@ class MainController < ApplicationController
       :body => params[:message],
       :via => :smtp,
       :via_options => {
-        :address => 'smtp.gmail.com',
+        :address => 'smtp.sendgrid.net',
         :port => '587',
         :domain => 'heroku.com',
-        :user_name => ENV['GMAIL_USERNAME'],
-        :password => ENV['GMAIL_PASSWORD'],
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
         :authentication => :plain,
         :enable_starttls_auto => true
         }
@@ -27,7 +27,7 @@ class MainController < ApplicationController
     
     Pony.mail(options)
     
-    redirect ('/')
+    # redirect ('/')
   end
 
 end
